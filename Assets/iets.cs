dc0f75bcb9c91bffeada19f369bfd45cfafe5878
Jakub <jakubszymanzki@gmail.com>
2025-12-03 15:09:27 +0100

    fixed some things and added the line renderer and forced a 4:3 aspect ratio to make it more lore accurate

--- Assets/iets.cs
+++ Assets/iets.cs
@@ -2,30 +2,26 @@ using UnityEngine;
 
 public class RandomItem : MonoBehaviour
 {
-    [SerializeField] string[] Items;
+    [SerializeField] string[] item;
     void Start()
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return)) PrintRandomItem();
         if (Input.GetKeyDown(KeyCode.Escape)) PrintAllItems();
     }
-
     private void PrintRandomItem()
     {
-        int i = Random.Range(0, Items.Length);
-        Debug.Log(Items[i]);
+        int i = Random.Range(0, item.Length);
+        Debug.Log(item[i]);
     }
-
     private void PrintAllItems()
     {
-        for (int i = 0; i < Items.Length; i++)
+        for (int i = 0; i < item.Length; i++)
         {
-            Debug.Log(Items[i]);
+            Debug.Log(item[i]);
         }
     }
 }
